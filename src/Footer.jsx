cae65c226e0c80fa10085a3b28d11f550729e831
Jatin <jatinverma2428@gmail.com>
2026-08-27 12:40:43 +0530

    push

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -37,7 +37,7 @@ export default function Footer() {
       delay: 2000,
     },
     {
-      href: "https://github.com/Jatin136900",
+      href: "https://github.com/Jatin9785",
       icon: <FaGithub />,
       label: "GitHub",
       color: "#fff",
